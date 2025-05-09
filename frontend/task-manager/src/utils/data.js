be8df6078d9d14cbb8d80991e7ccb81500903be6
Prasad kodre <kodreprasad2002@gmail.com>
2025-05-09 23:15:16 +0530

    helloo commit

--- frontend/task-manager/src/utils/data.js
+++ frontend/task-manager/src/utils/data.js
@@ -11,7 +11,7 @@ export const SIDE_MENU_DATA = [
     id: "01",
     label: "Dashboard",
     icon: LuLayoutDashboard,
-    path: "/admin/dashboard",
+    path: "/user/dashboard",
   },
   {
     id: "02",
