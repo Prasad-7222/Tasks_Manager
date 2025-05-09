026aab2e94076e3a544bd45463269c58bef3c853
Prasad kodre <kodreprasad2002@gmail.com>
2025-05-10 00:11:44 +0530

    helloo commit

--- frontend/task-manager/src/components/layouts/Navbar.jsx
+++ frontend/task-manager/src/components/layouts/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ activeMenu }) => {
           <HiOutlineMenu className="text-2xl" />
         )}
       </button>
-      <h2 className="text-lg font-medium text-block">Task Manager</h2>
+      <h2 className="text-lg font-medium text-block">TaskPro</h2>
       {openSideMenu && (
         <div className="fixed top-[61px] -ml-4 bg-white">
           <SideMenu activeMenu={activeMenu} />
